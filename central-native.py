ea2afadc81e90283bc0da41741fec5967f74b821
Lexographic <68760578+Lexographic@users.noreply.github.com>
2021-08-31 19:17:01 -0500

    Add files via upload

---
+++ central-native.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import statistics
+
+
+# In[2]:
+
+
+values = [47, 95, 88, 73, 88, 84]
+
+
+# In[3]:
+
+
+statistics.mean(values)
+
+
+# In[4]:
+
+
+statistics.median(values)
+
+
+# In[5]:
+
+
+statistics.mode(values)
+
+
+# In[ ]:
+
+
+
+
---
+++ central-with-statistics.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+values = [47, 95, 88, 73, 88, 84]
+
+
+# In[2]:
+
+
+sum(values) / len(values)
+
+
+# In[3]:
+
+
+import statistics
+
+
+# In[4]:
+
+
+statistics.mean(values)
+
+
+# In[5]:
+
+
+statistics.median(values)
+
+
+# In[6]:
+
+
+statistics.mode(values)
+
+
+# In[7]:
+
+
+sorted(values)
+
+
+# In[ ]:
+
+
+
+
